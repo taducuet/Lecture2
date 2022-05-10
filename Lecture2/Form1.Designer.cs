2949d4ba3c8ae47f9e30aea8dc0b620d26645634
taduc <taduc@DUCTA>
2022-05-10 09:11:10 +0700

    Add project files.

---
+++ Lecture2/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿
+namespace Lecture2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.btnSplit = new System.Windows.Forms.Button();
+            this.btnCvt = new System.Windows.Forms.Button();
+            this.btnSobel = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnGauss = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(331, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(13, 319);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(85, 37);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "Open File";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(105, 319);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(207, 37);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "\"C:\\Users\\taduc\\OneDrive\\Desktop\\images\\0010001.png\"";
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(13, 387);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(131, 55);
+            this.btnShow.TabIndex = 3;
+            this.btnShow.Text = "Show Image";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // btnSplit
+            // 
+            this.btnSplit.Location = new System.Drawing.Point(181, 387);
+            this.btnSplit.Name = "btnSplit";
+            this.btnSplit.Size = new System.Drawing.Size(131, 55);
+            this.btnSplit.TabIndex = 3;
+            this.btnSplit.Text = "Split Image";
+            this.btnSplit.UseVisualStyleBackColor = true;
+            this.btnSplit.Click += new System.EventHandler(this.btnSplit_Click);
+            // 
+            // btnCvt
+            // 
+            this.btnCvt.Location = new System.Drawing.Point(12, 448);
+            this.btnCvt.Name = "btnCvt";
+            this.btnCvt.Size = new System.Drawing.Size(131, 55);
+            this.btnCvt.TabIndex = 3;
+            this.btnCvt.Text = "Convert to Gray";
+            this.btnCvt.UseVisualStyleBackColor = true;
+            this.btnCvt.Click += new System.EventHandler(this.btnCvt_Click);
+            // 
+            // btnSobel
+            // 
+            this.btnSobel.Location = new System.Drawing.Point(181, 448);
+            this.btnSobel.Name = "btnSobel";
+            this.btnSobel.Size = new System.Drawing.Size(131, 55);
+            this.btnSobel.TabIndex = 3;
+            this.btnSobel.Text = "Sobel";
+            this.btnSobel.UseVisualStyleBackColor = true;
+            this.btnSobel.Click += new System.EventHandler(this.btnSobel_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(12, 509);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(131, 55);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "Delete Image";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnGauss
+            // 
+            this.btnGauss.Location = new System.Drawing.Point(181, 509);
+            this.btnGauss.Name = "btnGauss";
+            this.btnGauss.Size = new System.Drawing.Size(131, 55);
+            this.btnGauss.TabIndex = 3;
+            this.btnGauss.Text = "Gauss Filter";
+            this.btnGauss.UseVisualStyleBackColor = true;
+            this.btnGauss.Click += new System.EventHandler(this.btnGauss_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(392, 327);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(39, 21);
+            this.radioButton1.TabIndex = 4;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "R";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(456, 327);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(40, 21);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "G";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(508, 327);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(38, 21);
+            this.radioButton3.TabIndex = 4;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "B";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(572, 327);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(59, 21);
+            this.radioButton4.TabIndex = 4;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "RGB";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(331, 386);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(278, 56);
+            this.trackBar1.TabIndex = 5;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(631, 406);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(643, 579);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.btnSplit);
+            this.Controls.Add(this.btnGauss);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnSobel);
+            this.Controls.Add(this.btnCvt);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.Button btnSplit;
+        private System.Windows.Forms.Button btnCvt;
+        private System.Windows.Forms.Button btnSobel;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnGauss;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Lecture2/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+
+
+namespace Lecture2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        Mat img = new Mat();
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.RestoreDirectory = true;
+            file.CheckFileExists = false;
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = file.FileName;
+            }
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            //read image
+            img = Cv2.ImRead(textBox1.Text);
+            //Show image on PictureBox
+            pictureBox1.Image = img.ToBitmap();
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+
+        private void btnCvt_Click(object sender, EventArgs e)
+        {
+            Mat img_gray = new Mat();
+            Cv2.CvtColor(img, img_gray, ColorConversionCodes.BGR2GRAY);
+            pictureBox2.Image = img_gray.ToBitmap();
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+            pictureBox2.Image = null;
+        }
+
+        Mat[] RGB = new Mat[3];
+
+        private void btnSplit_Click(object sender, EventArgs e)
+        {
+            
+            RGB = Cv2.Split(img);
+            radioButton1.Enabled = true;
+            radioButton2.Enabled = true;
+            radioButton3.Enabled = true;
+            radioButton4.Enabled = true;
+            pictureBox2.Image = img.ToBitmap();
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            MessageBox.Show("Split Successful");
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            pictureBox2.Image = RGB[0].ToBitmap();
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            pictureBox2.Image = RGB[1].ToBitmap();
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            pictureBox2.Image = RGB[2].ToBitmap();
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            pictureBox2.Image = img.ToBitmap();
+        }
+
+        private void btnSobel_Click(object sender, EventArgs e)
+        {
+            Mat img_graySobel = new Mat();
+            Cv2.CvtColor(img, img_graySobel, ColorConversionCodes.BGR2GRAY);
+            Mat img_Sobel = new Mat();
+            Cv2.Sobel(img_graySobel, img_Sobel, -1, 1, 0);
+            pictureBox2.Image = img_Sobel.ToBitmap();
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+
+        private void btnGauss_Click(object sender, EventArgs e)
+        {
+            Mat img_grayGauss = new Mat();
+            Cv2.CvtColor(img, img_grayGauss, ColorConversionCodes.BGR2GRAY);
+            Mat img_Gauss = new Mat();
+            Cv2.GaussianBlur(img_grayGauss, img_Gauss, new OpenCvSharp.Size(3, 3), 1);
+            pictureBox2.Image = img_Gauss.ToBitmap();
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+
+        float gamma = 0;
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+        //    label1.Text = trackBar1.Value.ToString();
+        //    float value1 = 0.4f;
+
+        }
+    }
+}
